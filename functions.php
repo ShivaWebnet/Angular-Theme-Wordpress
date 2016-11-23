d4ec600017b632702adfe4634c790560fa6a15ec
ShivaWebnet <shivawebnet@gmail.com>
2016-11-23 16:57:39 +0100

    enrutar angular en functions.php y añadir wp_head

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+function my_scripts(){
+  wp_enqueue_script(
+    'angularjs',
+    get_stylesheet_directory_uri() . '/node_modules/angular/angular.min.js'
+  );
+
+  wp_enqueue_script(
+    'angular-route',
+    get_stylesheet_directory_uri() . '/node_modules/angular-route/angular-route.min.js'
+  );
+}
+
+
+add_action('wp_enqueue_script', 'my_scripts');
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html ng-app>
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Rock Paper Scissors</title>
+
+  <?php wp_head(); ?>
 </head>
 <body>
 
